f8b6b1045ae9c4641909d6e42981d07ff01b4f7c
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-18 17:09:51 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,5 @@
 # code your #valid_move? method here
-def valid_move? (board, index)
-if index > 8 || index < 1
-    false
-  elsif position_taken?(board, index)
-    false
-  elsif !position_taken?(board, index)
-    true
-  end
-end
+position_taken? ([], 0)
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken? (board, index)
