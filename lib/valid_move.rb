6258efe4243a1cfacc10cb69192ce92d9b21977b
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-18 17:02:04 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,9 @@
 # code your #valid_move? method here
 def valid_move? (board, index)
-  if position_taken?(board, index)
+  if position_taken?(board, index) || (index < 1 || index > 8 )
     false
   elsif !position_taken?(board, index)
     true
-  elsif index < 1 || index > 8
-    nil
   end
 end
 
