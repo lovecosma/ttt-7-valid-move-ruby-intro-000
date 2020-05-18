95254aedddcc73e2ca6db58d0f1512eeec95cb66
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-18 16:59:52 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move? (board, index)
   if position_taken?(board, index)
     false
-  elsif position_taken?(board, index) == false
+  elsif !position_taken?(board, index)
     true
   elsif index < 1 || index > 8
     false
